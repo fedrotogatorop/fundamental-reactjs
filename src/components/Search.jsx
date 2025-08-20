0f348ea74a7e6bf0fe0e2686a3238294e7b5af30
Fedro Togatorop <fedrotoga@gmail.com>
2025-08-20 15:16:55 +0700

    menambahkan fitur find in Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,15 +2,25 @@ import { useState } from "react";
 
 function Search(props) {
   const [search, setSearch] = useState("");
-  const onSearchChange = (event) => {
-    setSearch(event.target.value);
-    props.onSearchChange(event.target.value);
+  const onSearchChange = () => {
+    props.onSearchChange(search);
   };
-
+  // const onSearchChange = () => {
+  //   console.log("test");
+  // };
+  const searchKeydown = (e) => {
+    if (e.key === "Enter") {
+      onSearchChange();
+    }
+  };
   return (
     <>
       <div>
-        Cari artikel : <input onChange={onSearchChange}></input>
+        Cari artikel :{" "}
+        <input
+          onChange={(e) => setSearch(e.target.value)}
+          onKeyDown={searchKeydown}></input>
+        <button onClick={onSearchChange}>Find</button>
       </div>
       <small>
         Ditemukan {props.totalPosts} data dengan pencarian kata {search}
